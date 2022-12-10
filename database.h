6e3cd91bf917195d1974fb71c920440823468526
Chih-Yang <chihyanghsu0805@gmail.com>
2022-12-10 18:42:17 -0500

    update database

---
+++ database.h
@@ -0,0 +1,29 @@
+// Copyright [year] <Copyright Owner>"  [legal/copyright]
+#ifndef DATABASE_H_
+#define DATABASE_H_
+
+#include <mysql.h>
+
+#include <string>
+
+namespace database {
+
+class Database {
+  MYSQL* db_conn;
+  MYSQL_RES* res_set;
+  MYSQL_ROW row;
+
+ public:
+  void connect(const std::string,  // host
+               const std::string,  // user
+               const std::string,  // password
+               const std::string,  // database
+               const int);         // port
+  // Table Object?
+  void add_book();
+  void view_book();
+  void search_book();
+};
+
+}  // namespace database
+#endif  // DATABASE_H_
