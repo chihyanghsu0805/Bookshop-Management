d689ea597806fde61f0106d787736abae86428cc
Chih-Yang <chihyanghsu0805@gmail.com>
2022-12-10 18:47:36 -0500

    update book

---
+++ book.h
@@ -0,0 +1,12 @@
+// Copyright [year] <Copyright Owner>"  [legal/copyright]
+#ifndef BOOK_H_
+#define BOOK_H_
+
+#include "./database.h"
+
+namespace book {
+
+void menu(database::Database*);
+
+}  // namespace book
+#endif  // BOOK_H_
